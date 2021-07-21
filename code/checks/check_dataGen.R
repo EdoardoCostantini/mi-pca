311a79304c7a73e4ffe67e21c14c5cf84a63792c
EdoardoCostantini <38205413+EdoardoCostantini@users.noreply.github.com>
2021-07-21 13:43:44 +0200

    checks: improved checks of correct model generation

--- code/checks/check_dataGen.R
+++ code/checks/check_dataGen.R
@@ -2,7 +2,7 @@
 ### Project:  MI-PCA study
 ### Author:   Edoardo Costantini
 ### Created:  2021-07-20
-### Modified: 2021-07-20
+### Modified: 2021-07-21
 
 ## Make sure we have a clean environment:
 rm(list = ls())
@@ -46,3 +46,71 @@ store_cors_ob[to_check]
   # change due to the discreteness factor. Therefore, we cannot use it
   # because it would lead to an effect on the outcomes (attenutaion of MAR,
   # gets closer to MCAR, imputaion matters less)
+
+# Factor Loadings Generated and recovered ---------------------------------
+
+  rm(list=ls())
+  source("./init.R")
+
+  set.seed(20210127)
+  cond <-  conds[446, ] # condition with continuous variables
+  cond <-  conds[2, ] # condition with continuous variables
+
+  res <- NULL
+  for (i in 1:nrow(conds)){
+    cond <- conds[i, ]
+    for(rep in 1:10){
+      print(paste0("Cond: ", i, " Rep: ", rep))
+      # Gen data
+      dat_list <- genData(parms = parms, cond = cond)
+      Xy <- dat_list$dat_ob
+
+      # Define CFA model
+      n_items <- length(c(parms$varMap$ta, parms$varMap$mp))
+      lv_items <- split(x = paste0("z", 1:(n_items*parms$J)),
+                        f = rep(c(parms$varMap$ta, parms$varMap$mp),
+                                each = parms$J))
+      lv_models <- sapply(1:length(lv_items), function(it){
+        paste0("l", it,
+               " =~ ",
+               paste0(lv_items[[it]], collapse = " + ")
+        )
+      }
+      )
+      CFA_model <- paste(lv_models, collapse = "\n")
+
+      # Fit CFA model and check factor loadings
+      if("factor" %in% sapply(Xy[, unlist(lv_items)], class)){
+        temp_index <- which(sapply(Xy[, unlist(lv_items)], class) %in% "factor")
+        ordered_items <- colnames(Xy[, temp_index, drop = FALSE])
+      } else {
+        ordered_items <- NULL
+      }
+
+      fit <- cfa(CFA_model, data = Xy,
+                 ordered = ordered_items,
+                 std.lv = TRUE)
+
+      # Extract Parameter estiamtes
+      CFA_par <- parameterEstimates(fit,
+                                    se = FALSE, zstat = FALSE,
+                                    pvalue = FALSE, ci = FALSE,
+                                    standardized = TRUE)
+      par_all <- cbind(par = apply(CFA_par[, 1:3], 1, paste0, collapse = ""),
+                       CFA_par[, c(6), drop = FALSE])
+
+      # Store Results
+      rep_res <- c(fal = mean(par_all[grep("=~", par_all$par), "std.all"]),
+                   erv = mean(par_all[grep("z\\d~~", par_all$par), "std.all"]),
+                   lcv = par_all[grep("l1~~l2", par_all$par), "std.all"])
+      res <- rbind(res, data.frame(c(cond, rep_res)))
+    }
+  }
+
+summar <- res %>%
+  group_by(tag) %>%
+  dplyr::summarize(fal = mean(fal),
+                   erv = mean(erv),
+                   lcv = mean(lcv))
+
+lapply(summar[, -1], function (x) boxplot(x))

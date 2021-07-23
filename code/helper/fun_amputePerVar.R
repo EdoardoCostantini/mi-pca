d04ae293021b6d9b617bd2c556960f792e28e7e5
EdoardoCostantini <38205413+EdoardoCostantini@users.noreply.github.com>
2021-07-23 12:12:58 +0200

    feature: amputePerVar() retunrs correct output + description

--- code/helper/fun_amputePerVar.R
+++ code/helper/fun_amputePerVar.R
@@ -6,18 +6,25 @@
 
 amputePerVar <- function(targets, preds, pm = .5, type = "high"){
   ## Description
-  # Adds Matrix Design missingness to the MAR
+  # Uses the simMissingness() function to impose missing values on
+  # targets set of variables based on preds.
+  # It returns the variables in targets with missing values imposed.
   ## Example Inputs
   # cond <- conds[1, ]
   # dat_list <- genData(parms = parms, cond = cond)
   # targets = dat_list$dat_ob[, parms$varMap_items$ta]
   # preds = dat_list$dat_ob[, parms$varMap_items$mp]
+  # preds = dat_list$dat_lv[, parms$varMap$mp,
+  #                           drop = FALSE]
   # pm = parms$pm
   # type = "high"
 
+  ## Body
+  preds_object <- model.matrix(~ ., as.data.frame(preds))[, -1, drop = FALSE]
+
   for (i in parms$varMap_items$ta) {
     nR <- simMissingness(pm    = pm,
-                         data  = model.matrix(~ ., preds)[, -1],
+                         data  = preds_object,
                          type  = type)
     
     # Fill in NAs
@@ -25,5 +32,5 @@ amputePerVar <- function(targets, preds, pm = .5, type = "high"){
   }
 
   # Result
-  return( target )
+  return( targets )
 }

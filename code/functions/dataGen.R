f37cc74a3760ac272f2a946df8e56f2a7387853e
Edo <38205413+EdoardoCostantini@users.noreply.github.com>
2021-09-06 17:42:01 +0200

    sim: dataGen() offer simple set up for continuous and ordinal generation

--- code/functions/dataGen.R
+++ code/functions/dataGen.R
@@ -2,7 +2,7 @@
 ### Project:  MI-PCA study
 ### Author:   Edoardo Costantini
 ### Created:  2021-05-20
-### Modified: 2021-08-25
+### Modified: 2021-09-06
 
 genData <- function(parms, cond){
 
@@ -36,6 +36,20 @@ genData <- function(parms, cond){
   # Make symmetric
   Phi[upper.tri(Phi)] <- t(Phi)[upper.tri(Phi)]
 
+  # Sample from MVN
+  x <- mvrnorm(n = parms$N,
+               mu = rep(0, parms$P),
+               Sigma = Phi)
+
+  colnames(x) <- paste0("z", 1:ncol(x))
+
+  # Scale it correctly
+  x_scaled <- apply(x, 2, function(j) j*sqrt(parms$item_var))
+  x_center <- x_scaled + parms$item_mean
+  x_cont <- data.frame(x_center)
+
+  # Discretise it
+  x_ordi <- x_cont
   index_discrete <- c(
     # ta = tail(parms$vmap_it$ta,
     #           length(parms$vmap_it$ta) * cond$D),
@@ -44,72 +58,17 @@ genData <- function(parms, cond){
     ax = tail(parms$vmap$ax,
               length(parms$vmap$ax) * cond$D)
   )
-  n_ordi <- length(index_discrete)
-  n_cont <- parms$P-n_ordi
-
-  # Define marginals
-  quantiles <- seq(-2, 2, length.out = 1e3) # from a normal distribution
-  chunk_size <- 1e3/cond$K # size of chunks
-  quntiles_groups <- split(quantiles,
-                           ceiling(seq_along(quantiles)/chunk_size))
-  bounds <- sapply(quntiles_groups, min)[-1]
-  marginal <- round(pnorm(bounds), 4)
-  marginal_list <- as.list(
-    as.data.frame(
-      matrix(rep(marginal, (cond$K - 1) * n_ordi),
-             nrow = (cond$K - 1), ncol = n_ordi)
-    )
-  )
-  # min(abs(unlist(valid.limits(marginal_list, n_ordi,n_cont))))
-
-  if(cond$K > 2){
-
-    # Using package for correction of matrix
-    cmat <- OrdNor::cmat.star(marginal_list, Phi,
-                              no.ord = n_ordi,
-                              no.norm = n_cont)
-
-    # Obtain Sample
-    x <- genOrdNor(n = parms$N,
-                   plist = marginal_list,
-                   cmat.star = cmat,
-                   no.ord = n_ordi,
-                   mean.vec = rep(0, n_cont),
-                   sd.vec = rep(1, n_cont),
-                   no.norm = n_cont)
-  } else {
-    sigma.star <- compute.sigma.star(no.bin = n_ordi,
-                                     no.nor = n_cont,
-                                     prop.vec.bin = unlist(marginal_list),
-                                     corr.mat = Phi)
-
-    x <- jointly.generate.binary.normal(parms$N,
-                                        no.bin = n_ordi,
-                                        no.nor = n_cont,
-                                        prop.vec.bin = unlist(marginal_list),
-                                        mean.vec.nor = rep(0, n_cont),
-                                        var.nor = rep(1, n_cont),
-                                        sigma_star = sigma.star$sigma_star,
-                                        continue.with.warning = TRUE)
+  for(j in index_discrete){
+    x_ordi[, j] <- cut(x_cont[, j],
+                       breaks = cond$K,
+                       labels = 1:cond$K,
+                       ordered_result = TRUE)
   }
-
-# Rescale Observed Scores -------------------------------------------------
-
-  x_scaled <- apply(x[, parms$vmap$ta], 2, function(j) j*sqrt(parms$item_var))
-  x_center <- x_scaled + parms$item_mean
-  x[, parms$vmap$ta] <- x_center
-  x <- data.frame(x)
-
-# Transform into factors ordinal items -------------------------------------------------
-
-  index_factor <- sapply(x, function (j) {length(unique(j)) <= 10})
-  x[, index_factor] <- data.frame(lapply(x[, index_factor], function (j) {factor(j, ordered = TRUE)}))
-
-# Give meaningful names ---------------------------------------------------
-
-  colnames(x) <- paste0("z", 1:ncol(x))
   
 # Return Output -----------------------------------------------------------
-  
-  return(x)
+
+  return(
+    list(cont = x_cont,
+         ordi = x_ordi)
+  )
 }

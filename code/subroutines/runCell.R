8f3f6e5877fc10af07a010ab2d56145ed6906b8f
Edo <38205413+EdoardoCostantini@users.noreply.github.com>
2021-10-01 18:05:56 +0200

    feature: tryCatch in the runCell() script as well

--- code/subroutines/runCell.R
+++ code/subroutines/runCell.R
@@ -9,137 +9,151 @@
 
 runCell <- function(rp, cond, fs, parms) {
 
-# Example Internals -------------------------------------------------------
+  # Example Internals -------------------------------------------------------
 
   # cond = conds[18, ]
   # rp   = 1
+  tryCatch({
+    ### START TRYCATCH EXPRESSION
+    # Data Generation ---------------------------------------------------------
 
-# Data Generation ---------------------------------------------------------
+    dat <- genData(parms = parms, cond = cond)
 
-  dat <- genData(parms = parms, cond = cond)
+    ## Impose Missingness
+    preds   <- dat$cont[, parms$vmap$mp, drop = FALSE]
+    targets <- dat$ordi[, parms$vmap$ta, drop = FALSE]
+    target_miss <- amputePerVar(targets = targets,
+                                preds = preds,
+                                pm = parms$pm,
+                                type = "high")
+    dat_miss <- cbind(target_miss, dat$ordi[, -parms$vmap$ta])
 
-  ## Impose Missingness
-  preds   <- dat$cont[, parms$vmap$mp, drop = FALSE]
-  targets <- dat$ordi[, parms$vmap$ta, drop = FALSE]
-  target_miss <- amputePerVar(targets = targets,
-                              preds = preds,
-                              pm = parms$pm,
-                              type = "high")
-  dat_miss <- cbind(target_miss, dat$ordi[, -parms$vmap$ta])
+    # Imputation --------------------------------------------------------------
 
-# Imputation --------------------------------------------------------------
+    if(cond$method == "all"){
+      imp_out <- imputePCAall(dat_miss,
+                              imp_target = parms$vmap$ta,
+                              pcs_target = unlist(parms$vmap, use.names = FALSE),
+                              ncfs = cond$npc,
+                              parms = parms)
+    }
+    if(cond$method == "aux") {
+      imp_out <- imputePCAaux(dat_miss___,
+                              imp_target = parms$vmap$ta,
+                              pcs_target = c(parms$vmap$mp, parms$vmap$ax),
+                              ncfs = cond$npc,
+                              parms = parms)
+    }
+    if(cond$method == "vbv") {
+      imp_out <- imputePCAvbv(dat_miss,
+                              ncfs = cond$npc,
+                              parms = parms)
+    }
 
-  if(cond$method == "all"){
-    imp_out <- imputePCAall(dat_miss,
+    # MICE w/ optimal choices
+    if(cond$method == "MIOP") {
+      imp_out <- imputeMICE(Z = dat_miss,
                             imp_target = parms$vmap$ta,
-                            pcs_target = unlist(parms$vmap, use.names = FALSE),
-                            ncfs = cond$npc,
+                            preds = c(parms$vmap$ta,
+                                      parms$vmap$mp,
+                                      # auxiliaries that are not junk
+                                      parms$vmap$ax[
+                                        !parms$vmap$ax
+                                          %in%
+                                          dat$index_junk_aux
+                                      ]),
                             parms = parms)
-  }
-  if(cond$method == "aux") {
-    imp_out <- imputePCAaux(dat_miss,
+    }
+
+    # MICE w/ oracle knowledge
+    if(cond$method == "MIOR") {
+      imp_out <- imputeMICE(Z = dat_miss,
                             imp_target = parms$vmap$ta,
-                            pcs_target = c(parms$vmap$mp, parms$vmap$ax),
-                            ncfs = cond$npc,
+                            preds = c(parms$vmap$ta,
+                                      parms$vmap$mp),
                             parms = parms)
-  }
-  if(cond$method == "vbv") {
-    imp_out <- imputePCAvbv(dat_miss,
-                            ncfs = cond$npc,
+    }
+
+    # MICE w/ minimal missing data models (minimal)
+    if(cond$method == "MIMI") {
+      imp_out <- imputeMICE(Z = dat_miss,
+                            imp_target = parms$vmap$ta,
+                            preds = parms$vmap$ta,
                             parms = parms)
-  }
+    }
 
-  # MICE w/ optimal choices
-  if(cond$method == "MIOP") {
-    imp_out <- imputeMICE(Z = dat_miss,
-                          imp_target = parms$vmap$ta,
-                          preds = c(parms$vmap$ta,
-                                    parms$vmap$mp,
-                                    # auxiliaries that are not junk
-                                    parms$vmap$ax[
-                                      !parms$vmap$ax
-                                        %in%
-                                        dat$index_junk_aux
-                                    ]),
-                          parms = parms)
-  }
+    # Analyze and pool --------------------------------------------------------
 
-  # MICE w/ oracle knowledge
-  if(cond$method == "MIOR") {
-    imp_out <- imputeMICE(Z = dat_miss,
-                          imp_target = parms$vmap$ta,
-                          preds = c(parms$vmap$ta,
-                                    parms$vmap$mp),
-                          parms = parms)
-  }
+    if(cond$method %in% c("all", "aux", "vbv", "MIOP", "MIOR", "MIMI")){
+      ## Estimate Mean, variance, covariance
+      fits <- fitSatModel(mids = imp_out$mids,
+                          model = genLavaanMod(dat_miss,
+                                               targets = parms$vmap$ta)
+      )
 
-  # MICE w/ minimal missing data models (minimal)
-  if(cond$method == "MIMI") {
-    imp_out <- imputeMICE(Z = dat_miss,
-                           imp_target = parms$vmap$ta,
-                           preds = parms$vmap$ta,
-                           parms = parms)
-  }
+      ## Pool mean, variance, covariance
+      pooled_sat <- poolSatMod(fits)
 
-# Analyze and pool --------------------------------------------------------
+      ## Estimate and pool regression coefficients
+      pooled_cor <- poolCor(imp_out$mids, targets = parms$vmap$ta)
 
-  if(cond$method %in% c("all", "aux", "vbv", "MIOP", "MIOR", "MIMI")){
-    ## Estimate Mean, variance, covariance
-    fits <- fitSatModel(mids = imp_out$mids,
-                        model = genLavaanMod(dat_miss,
-                                             targets = parms$vmap$ta)
-    )
+      ## Join outputs
+      res <- rbind(pooled_sat, pooled_cor)
+    }
 
-    ## Pool mean, variance, covariance
-    pooled_sat <- poolSatMod(fits)
+    ## Complete Case analysis
+    if(cond$method == "CC"){
+      res <- fitSingleData(na.omit(dat_miss), targets = parms$vmap$ta)
+    }
 
-    ## Estimate and pool regression coefficients
-    pooled_cor <- poolCor(imp_out$mids, targets = parms$vmap$ta)
+    ## Original data
+    if(cond$method == "OG"){
+      res <- fitSingleData(dat$cont, targets = parms$vmap$ta)
+    }
 
-    ## Join outputs
-    res <- rbind(pooled_sat, pooled_cor)
-  }
+    ## Define explained variance information
+    if(cond$method %in% c("all", "aux", "vbv")){
+      PC_exp <- imp_out$CPVE
+    } else {
+      PC_exp <- NA
+    }
 
-  ## Complete Case analysis
-  if(cond$method == "CC"){
-    res <- fitSingleData(na.omit(dat_miss), targets = parms$vmap$ta)
-  }
+    ## Attach condition tags
+    res <- cbind(rp = rp, cond, PC_exp = PC_exp, res)
 
-  ## Original data
-  if(cond$method == "OG"){
-    res <- fitSingleData(dat$cont, targets = parms$vmap$ta)
-  }
+    # Store Output ------------------------------------------------------------
 
-  ## Define explained variance information
-  if(cond$method %in% c("all", "aux", "vbv")){
-    PC_exp <- imp_out$CPVE
-  } else {
-    PC_exp <- NA
-  }
-
-  ## Attach condition tags
-  res <- cbind(rp = rp, cond, PC_exp = PC_exp, res)
-
-# Store Output ------------------------------------------------------------
-  
-  ## Store Main Results
-  saveRDS(res,
-          file = paste0(fs$outDir,
-                        "rep_", rp, "_", cond$tag,
-                        "_main",
-                        ".rds")
-  )
-
-  ## Store Cumulative Explained Variance in vbv case
-  if(cond$method == "vbv"){
-    pc_res <- cbind(cond, imp_out$CPVE)
-    saveRDS(pc_res,
+    ## Store Main Results
+    saveRDS(res,
             file = paste0(fs$outDir,
                           "rep_", rp, "_", cond$tag,
-                          "_CPVE",
+                          "_main",
                           ".rds")
     )
+
+    ## Store Cumulative Explained Variance in vbv case
+    if(cond$method == "vbv"){
+      pc_res <- cbind(cond, imp_out$CPVE)
+      saveRDS(pc_res,
+              file = paste0(fs$outDir,
+                            "rep_", rp, "_", cond$tag,
+                            "_CPVE",
+                            ".rds")
+      )
+    }
+    ### END TRYCATCH EXPRESSION
+  }, error = function(e){
+    err <- paste0("Original Error: ", e)
+    err_res <- cbind(rp = rp, cond, Error = err)
+    saveRDS(err_res,
+            file = paste0(fs$outDir,
+                          "rep_", rp, "_", cond$tag,
+                          "_ERROR",
+                          ".rds")
+    )
+    return(NULL)
   }
+  )
 
 }
 

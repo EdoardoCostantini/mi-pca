18aa2830e41ff52930dd9681503a61dc73f65712
Edo <38205413+EdoardoCostantini@users.noreply.github.com>
2021-11-15 10:23:14 +0100

    test: improve checking of genDataLatent() results
    You can now
    - check the presence of the block structure in the correlation matrix
    - check the correlation between these paris of items: ta-ta, ta-mp, ta-ax, mp-mp, ax-ax

--- code/checks/check_dataGen.R
+++ code/checks/check_dataGen.R
@@ -2,7 +2,7 @@
 ### Project:  MI-PCA study
 ### Author:   Edoardo Costantini
 ### Created:  2021-07-20
-### Modified: 2021-09-03
+### Modified: 2021-11-15
 
 ## Make sure we have a clean environment:
 rm(list = ls())
@@ -41,19 +41,33 @@ conds <- expand.grid(K = K,
                      stringsAsFactors = FALSE)
 
 # Store correlations
-store_cors <- matrix(nrow = nrow(conds), ncol = 3)
+store_cors <- matrix(nrow = nrow(conds), ncol = 5,
+                     dimnames = list(NULL, c("ta_ta", "ta_mp", "ta_ax", "mp_mp", "ax_ax")))
 
 for(i in 1:nrow(conds)){
   cond <- conds[i, ]
   dat <- genDataLatent(parms = parms, cond = cond)
   cormat <- round(cor(dat$x), 3)
-  ta_cor <- cormat[parms$vmap$ta, parms$vmap$ta][upper.tri(diag(length(parms$vmap$ta)))]
-  mp_cor <- cormat[parms$vmap$mp, parms$vmap$mp][upper.tri(diag(length(parms$vmap$mp)))]
-  ax_cor <- cormat[parms$vmap$ax, parms$vmap$ax][upper.tri(diag(length(parms$vmap$ax)))]
-  store_cors[i, ] <- sapply(list(ta_cor, mp_cor, ax_cor), mean)
+  ta_ta <- cormat[parms$vmap$ta, parms$vmap$ta][upper.tri(diag(length(parms$vmap$ta)))]
+  ta_mp  <- cormat[parms$vmap$ta, parms$vmap$mp]
+  ta_ax  <- cormat[parms$vmap$ta, parms$vmap$ax]
+  mp_mp <- cormat[parms$vmap$mp, parms$vmap$mp][upper.tri(diag(length(parms$vmap$mp)))]
+  ax_ax <- cormat[parms$vmap$ax, parms$vmap$ax][upper.tri(diag(length(parms$vmap$ax)))]
+  store_cors[i, ] <- sapply(list(ta_ta = ta_ta,
+                                 ta_mp = ta_mp,
+                                 ta_ax = ta_ax,
+                                 mp_mp = mp_mp,
+                                 ax_ax = ax_ax), mean)
   dat_ordi <- disData(x = dat$x, K = cond$K, parms = parms) # no errors is enough
 }
-round(store_cors, 1)
+
+# Blocks
+round(cormat, 1)[(parms$P-13):parms$P, (parms$P-13):parms$P]
+  # Even auxiliary variables in the same block are correlated .7,
+  # but across blocks, they are not correlated
+
+# Trends
+cbind(conds, round(store_cors, 1))
 colMeans(round(store_cors, 1))
-
-
+  # Ta and mp variables are correlated .7 as in original study
+  # without latent variable structure

6afb1e8f8aed6042017b35afd2d47fb97a339d4b
Edo <38205413+EdoardoCostantini@users.noreply.github.com>
2023-03-02 16:46:26 +0100

    feat: make violin plots for the ngdr simulation study

--- code/pc_extra_step2_analyse.R
+++ code/pc_extra_step2_analyse.R
@@ -11,10 +11,12 @@
 
 # Load Results ------------------------------------------------------------
 
-  output <- readTarGz("20230302_145539.tar.gz")
+    inDir <- "../output/"
+    target_tar <- "20230302_153750.tar.gz"
+    output <- readTarGz(target_tar)
 
   # Collect main results
-  rds_main <- do.call(rbind, output$out)
+  rds_main <- do.call(rbind, output$out[-grep("ERROR", output$file_names)])
 
   # Read error results
   rds_error_names <- grep("ERROR", output$file_names)
@@ -29,9 +31,32 @@
 # Make value a number
 rds_main$value <- as.numeric(rds_main$value)
 
-# Take average of value across repetitions
-res <- rds_main %>% # Specify data frame
-    group_by(tag, P, K, D, interval, pj, lv, method, data) %>% # Specify group indicator
+# Make some variables factors
+rds_main$K <- factor(rds_main$K)
+rds_main$method <- factor(rds_main$method, levels = c("noc", "naf", "nkaiser", "nparallel"))
+rds_main$data <- factor(rds_main$data, levels = c("og", "na"), labels = c("Original data", "Data with NAs"))
+
+# Store the rds results
+saveRDS(
+    object = rds_main,
+    file = paste0(
+        inDir,
+        "extra_ngdr_gg_shape.rds"
+    )
+)
+
+# Subset the data as desired
+rds_main_sub <- rds_main %>%
+    filter(
+        lv == TRUE,
+        # K == Inf,
+        pj == 0,
+        P == 56
+    )
+
+# Take average of value across repetitions for conditions of interest
+res <- rds_main_sub %>%
+    group_by(tag, P, K, D, interval, pj, lv, method, data) %>%
     summarise(
         outcome = mean(value)
     )
@@ -39,30 +64,28 @@ res <- rds_main %>% # Specify data frame
 # Make result a data.frame
 res <- data.frame(res)
 
-# Make K a factor
-res$K <- factor(res$K)
-head(res)
+# Violin plots -----------------------------------------------------------------
 
-# Make plots that number of components per
-res_sub <- res %>%
-    filter(
-        lv == TRUE,
-        # K == Inf,
-        pj == 1,
-        P == 56
-    ) 
-res_sub %>%
-    ggplot(aes(x = method, y = outcome, label = factor(outcome))) +
-    geom_point() +
-    geom_label() +
+# Add violin plot on top
+ggplot(rds_main_sub, aes(factor(method), value)) +
+    geom_violin(
+        adjust = 2,
+        trim = TRUE
+    ) +
     facet_grid(
         rows = vars(K),
         cols = vars(data),
-        scales = "free",
-        switch = "y"
+        scales = "free"
+    ) +
+    theme_bw() +
+    geom_label(
+        data = res,
+        aes(x = method, y = outcome, label = factor(outcome))
     ) +
     coord_cartesian(
-        ylim = c(0, max(res_sub$outcome) + 5)
-    ) + 
-    theme_bw()
-    
+        ylim = c(0, max(res$outcome) + 5)
+    ) +
+    theme(
+        axis.title.x = element_blank(),
+        axis.title.y = element_blank()
+    )
